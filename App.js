792843960df3b1c9904e76edb284241dda1316a7
theshifaligwal <gwal.shifali2223@gmail.com>
2021-12-05 07:58:13 +0530

    Create header component

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Import component
+import Header from './components/Header';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world!</Text>
-      <StatusBar style="auto" />
+      <Header title="Shopping List" />
     </View>
   );
 }
@@ -14,8 +16,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 60,
   },
 });
---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Header(props) {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>{props.title}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    padding: 15,
+    backgroundColor: "darkslateblue",
+    height: 60,
+  },
+
+  text: {
+    color: "#fff",
+    fontSize: 23,
+    textAlign: "center",
+  },
+});
