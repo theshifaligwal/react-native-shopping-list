f1c27b432765a80b41b6a3f2a2f72e2e0e6fd4dc
theshifaligwal <gwal.shifali2223@gmail.com>
2021-12-05 12:16:35 +0530

    Add css in listItems

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,26 +1,35 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-const ListItem = ({item}) => {
+const ListItem = ({ item }) => {
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
-      <Text style={styles.listItemText}>{item.text}</Text>
-      <AntDesign name="close" size={24} color="red" />
+        <Text style={styles.listItemText}>{item.text}</Text>
+        <AntDesign name="close" size={24} color="red" />
       </View>
     </TouchableOpacity>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  listItem:{
-      padding: 15,
-      backgroundColor: '#f8f8f8',
+  listItem: {
+    padding: 15,
+    backgroundColor: "#f8f8f8",
+    borderBottomWidth: 1,
+    borderColor: "#eee",
+  },
+  listItemView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 
-  }
+  listItemText: {
+    fontSize: 18,
+  },
 });
-
 
 export default ListItem;
